e073579828791eba54e90563e5048e40329abc43
pipiloulou <pipiloulou@gmail.com>
2017-09-03 15:37:19 +0200

    Initialisation projet

---
+++ GestionBanque/src/Test.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;
+
+public class Test {
+
+	
+	
+
+	  private static Logger logger = Logger.getLogger(Test.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		logger.info("test de logg ");
+	}
+
+}
